d73da7a017d865f0a230ae7ddc19b35c23603ac5
Salomanka <oleg65play@gmail.com>
2024-11-15 14:25:44 +1000

    ss

--- build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
+++ build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -11,8 +11,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os/src/main.cpp" "CMakeFiles/HelloWorld.dir/src/main.cpp.obj" "gcc" "CMakeFiles/HelloWorld.dir/src/main.cpp.obj.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
