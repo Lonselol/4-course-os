81574185e772aa09f009490993d92382583e0642
Salomanka <oleg65play@gmail.com>
2024-11-22 02:41:11 +1000

    sh

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
+set -e
+
 git pull origin main  # Обновление кода из Git
+
+export CC=gcc
+export CXX=g++
+
 mkdir -p build
 cd build
-cmake ..
-make
+
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+
+cmake --build . --config Debug
+
+cd ..
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os
+# Install script for directory: /home/salomanka/GitHub/4-course-os
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/4-course-os")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,43 +27,36 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set path to fallback-tool for dependency-resolution.
+# Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Strawberry/c/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os/build/lab-helloworld/cmake_install.cmake")
-  include("C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os/build/lab-lib/cmake_install.cmake")
-  include("C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os/build/lab-lib-test/cmake_install.cmake")
+  include("/home/salomanka/GitHub/4-course-os/build/lab-helloworld/cmake_install.cmake")
+  include("/home/salomanka/GitHub/4-course-os/build/lab-lib/cmake_install.cmake")
+  include("/home/salomanka/GitHub/4-course-os/build/lab-lib-test/cmake_install.cmake")
 
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os/build/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
 if(CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-  else()
-    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
-    unset(CMAKE_INST_COMP_HASH)
-  endif()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/YasperMoglot/Desktop/Ycheba/4course/4-course-os/build/${CMAKE_INSTALL_MANIFEST}"
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/salomanka/GitHub/4-course-os/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
