2374adf6c0a32e6910eed6bbd9c271e57a003685
Salomanka <oleg65play@gmail.com>
2024-11-22 04:49:51 +1000

    Update main.cpp

--- lab-helloworld/main.cpp
+++ lab-helloworld/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <Windows.h>
 
 using namespace std;
 
 int main() {
     while(true) {
-        Sleep(500);
         cout << "Hello world!" << endl;
     }
     return 0;
